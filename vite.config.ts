478bb755aebaad21c6c0e009d7cf8867c8bcd44e
LorienYang <yangluoying19277904322@outlook.com>
2025-12-01 15:57:27 +0800

    引入devtool

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
       vue(),
+      vueDevTools(),
+      createHtmlPlugin({}),
       tailwindcss(),
   ],
 })
